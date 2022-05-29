73ab0d258e0518f3685202e5bc58652acea3df5c
minjie <minjie@sj56.info>
2022-05-29 22:23:43 +0800

    feat: 新增分包

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,6 +2,20 @@ export default defineAppConfig({
   pages: [
     'pages/index/index'
   ],
+  subpackages: [
+    {
+      root: 'shadowA', // 分包1
+      pages: [
+        'pages/index/index'
+      ]
+    },
+    {
+      root: 'shadowB', // 分包2
+      pages: [
+        'pages/index/index'
+      ]
+    }
+  ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import { View, Text, Button } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 import { StoreProps } from '@store/index'
 import { request } from '@service/index'
 // import { StoreProps } from '../../store/index'
@@ -22,10 +23,19 @@ export default class Index extends Component<IndexProps> {
     })
   }
 
+  onTrunLink = (type: '/shadowA'|'/shadowB') => {
+    const url = type + '/pages/index/index'
+    Taro.navigateTo({ url })
+  }
+
   render () {
     const { counter } = this.props.store
     return (
       <View className='index'>
+        <View>
+          <Button onClick={() => this.onTrunLink('/shadowA')}>跳转shadowA</Button>
+          <Button onClick={() => this.onTrunLink('/shadowB')}>跳转shadowB</Button>
+        </View>
         <View>
           <Button onClick={this.onRquestURL}>request</Button>
         </View>
